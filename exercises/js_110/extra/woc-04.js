52f856c76b072aacb7e66a898672fff3550c603b
hamin <haminson@gmail.com>
2024-08-13 20:23:59 -0700

    Format line spacing for prob statement

--- exercises/js_110/extra/woc-04.js
+++ exercises/js_110/extra/woc-04.js
@@ -1,4 +1,8 @@
-/* Given 2 strings, your job is to find out if there is a substring that appears in both strings. You will return true if you find a substring that appears in both strings or false if you do not. We only care about substrings that are longer than one letter long. */
+/* Given 2 strings, your job is to find out if there is a substring
+ * that appears in both strings. You will return true if you find a
+ * substring that appears in both strings or false if you do not. We
+ * only care about substrings that are longer than one letter long.
+ * */
 
 /* [23min]
 IN: {str, str}
